552f02bb36680c92364ce5705be14634cb0ea628
Difegue <sugoi@cock.li>
2022-04-07 01:32:53 +0200

    Experiments with svg group hiding

--- LCDonald/Views/MainWindow.axaml.cs
+++ LCDonald/Views/MainWindow.axaml.cs
@@ -1,14 +1,41 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 using Avalonia.Svg.Skia;
+using Avalonia.Controls.Skia;
+using Svg;
+using Svg.Model;
 using SS = Svg.Skia;
+using SkiaSharp;
 
 namespace LCDonald.Views
 {
     public partial class MainWindow : Window
     {
+        private SKPictureControl svgElement;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            var svgDocument = SvgExtensions.Open("F:\\Projects\\LCDonald\\LCDonald\\Assets\\GameAssets\\tskyadventure\\tskyadventure.svg");
+
+
+            svgDocument.ApplyRecursive(e => {
+                if (e.ID == "life-1") 
+                    e.Visibility = "hidden";
+            });
+            svgElement = this.FindControl<SKPictureControl>("SvgTest");
+            
+            var svgSource = new SvgSource();
+            svgElement.Picture = svgSource.FromSvgDocument(svgDocument);
+                
+                  
+
         }
     }
 }
