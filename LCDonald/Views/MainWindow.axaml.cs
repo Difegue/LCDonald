a8ac54ec0b9f7c3b3c7ae454472255dd33ee4af6
Difegue <sugoi@cock.li>
2022-04-13 22:56:48 +0200

    Bare minimum in terms of presentation 🎊

--- LCDonald/Views/MainWindow.axaml.cs
+++ LCDonald/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia;
+using Avalonia.Input;
+using FluentAvalonia.UI.Controls;
+using System.Diagnostics;
 
 namespace LCDonald.Views
 {
@@ -18,7 +21,28 @@ namespace LCDonald.Views
 #if DEBUG 
         this.AttachDevTools();
 #endif
+        }
 
+        private async void Open_About(object sender, PointerPressedEventArgs e)
+        {
+            // TODO slappy implementation, should go in settings later
+            var dialog = new ContentDialog()
+            {
+                Title = "About McD's Sonic Simulator",
+                Content = "A monument to the everlasting symbiotic relationship between Sonic the Hedgehog and ultracapitalist fast food joints. 🏛\n\nPowered by Avalonia and the LCDonald engine. \nFeel free to contribute to the project on GitHub!",
+                PrimaryButtonText = "Ok",
+                SecondaryButtonText = "View Source Code"
+            };
+
+            var result = await dialog.ShowAsync();
+
+            // Open URL if secondary
+            if (result == ContentDialogResult.Secondary)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo("https://github.com/Difegue/LCDonald");
+                psi.UseShellExecute = true;
+                Process.Start(psi);
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+<img src="cover.png" width="196">
+
+LCDonald
+===========
+
+Simulator engine for reproducing LCD games made by McDonald's in the 2000's.  
+Mostly focused on simulating the [McDonald's Sonic LCD games](http://info.sonicretro.org/McDonald%27s_Sonic_LCD_games).  
+
+You can read more details about the technical stack [here](https://tvc-16.science/lcdonald.html).  
+
+Feel free to look at the [issues](https://github.com/Difegue/LCDonald/issues) if you want to help on a game!  
+
+## Features
+
+* SVG-based rendering for LCD elements
+* Uses the well-known [MAMELayout](https://docs.mamedev.org/techspecs/layout_files.html) format for screen positioning
+* [AvaloniaUI](http://avaloniaui.net/)-based frontend, runs on Windows/Mac/Linux -- Backend is pure .NET and can run on (mostly)anything!
+
+All scanned/photographied materials in this repository are ©️SEGA, 2004/2005/2006.
