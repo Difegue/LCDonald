dafcd18504c88233299bf0ac10dbf80e30b0671e
Difegue <sugoi@cock.li>
2023-02-09 21:49:30 +0100

    Migrate to split Avalonia project structure + add Android stub

---
+++ LCDonald.Desktop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using Avalonia;
+
+namespace LCDonald.Desktop;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .With(new Win32PlatformOptions { AllowEglInitialization = true })
+                .With(new SkiaOptions { MaxGpuResourceSizeBytes = 256000000 })
+                .LogToTrace();
+}
