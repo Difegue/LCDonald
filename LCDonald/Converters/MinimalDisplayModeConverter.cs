24112ad0c6795f267fa5234eab523898cb5e858c
Difegue <sugoi@cock.li>
2023-05-06 03:02:33 +0200

    Fix minimal display mode menu being hidden by game title

---
+++ LCDonald/Converters/MinimalDisplayModeConverter.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Data.Converters;
+using FluentAvalonia.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCDonald.Converters
+{
+    public class MinimalDisplayModeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is NavigationViewDisplayMode m)
+            {
+                return m != NavigationViewDisplayMode.Minimal; 
+            }
+            
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
